ea40d9fd992b1c4bb9d83c2ea6198ba836e311b0
Araf020 <Araf020@users.noreply.github.com>
2021-05-23 15:54:50 +0600

    Initial commit

---
+++ src/test/java/team/crapplet/investment_assistant/InvestmentAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package team.crapplet.investment_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
